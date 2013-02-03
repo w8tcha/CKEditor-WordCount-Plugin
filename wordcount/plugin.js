1dd87a1906e3e9eb28ca243791df99f0d87dcc00
Ingo Herbote <thewatcher@watchersnet.de>
2013-02-03 08:27:14 +0100

    fixed issue #1 + merged the changes from the sabf fork + moved the style code to an external css file

--- wordcount/plugin.js
+++ wordcount/plugin.js
@@ -3,9 +3,9 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
  
- CKEDITOR.plugins.add('wordcount', {
+CKEDITOR.plugins.add('wordcount', {
     lang: ['de', 'en'],
-	init: function (editor) {
+    init: function (editor) {
         var defaultLimit = 'unlimited';
         var defaultFormat = '<span class="cke_path_item">' + editor.lang.wordcount.WordCount + ' %count%</span>';
         var limit = defaultLimit;
@@ -14,81 +14,91 @@
         var lastCount = 0;
         var limitReachedNotified = false;
         var limitRestoredNotified = false;
-        if (true) {
-            function counterId(editor) {
-                return 'cke_wordcount_' + editor.name
-            }
-            function counterElement(editor) {
-				return document.getElementById(counterId(editor))
-            }
-            function strip(html) {
-                var tmp = document.createElement("DIV");
-                tmp.innerHTML = html;
-                return tmp.textContent || tmp.innerText
-            }
-            function updateCounter(editor) {
-                var count = 0;
-                if (editor.getData() != undefined) {
-                    count = strip(editor.getData()).trim().split(/\s+/).length;
-                }
-                if (count == lastCount) {
-                    return true
-                } else {
-                    lastCount = count
-                }
-                if (!limitReachedNotified && count > limit) {
-                    limitReached(editor)
-                } else if (!limitRestoredNotified && count < limit) {
-                    limitRestored(editor)
-                }
-                var html = format.replace('%count%', count);
-                counterElement(editor).innerHTML = html
-            }
-            function limitReached(editor) {
-                limitReachedNotified = true;
-                limitRestoredNotified = false;
-				editor.execCommand( 'undo' );
-				// lock editor
-				editor.config.Locked = 1;
-            }
-            function limitRestored(editor) {
-                limitRestoredNotified = true;
-                limitReachedNotified = false;
-				
-				editor.config.Locked = 0;
-			
-            }
-            editor.on('uiSpace', function (event) {
-				if (event.data.space == 'bottom') {
-                    event.data.html += '<div id="' + counterId(event.editor) + '" class="cke_wordcount" style="display:block;float:right;margin-top:5px;margin-right:3px;color:black;"' + ' title="' + CKEDITOR.tools.htmlEncode('Words Counter') + '"' + '>&nbsp;</div>'
-                }
-            }, editor, null, 100);
-            editor.on('instanceReady', function (event) {
-                if (editor.config.wordcount_limit != undefined) {
-                    limit = editor.config.wordcount_limit
-                }
-                if (editor.config.wordcount_format != undefined) {
-                    format = editor.config.wordcount_format
-                }
-            }, editor, null, 100);
-            editor.on('dataReady', function (event) {
-                var count = event.editor.getData().length;
-                if (count > limit) {
-                    limitReached(editor)
-                }
-                updateCounter(event.editor)
-            }, editor, null, 100);
-            editor.on('key', function (event) {}, editor, null, 100);
-            editor.on('focus', function (event) {
-                editorHasFocus = true;
-                intervalId = window.setInterval(function (editor) {
-                    updateCounter(editor)
-                }, 1000, event.editor)
-            }, editor, null, 100);
-            editor.on('blur', function (event) {
-                editorHasFocus = false;
-                if (intervalId) clearInterval(intervalId)
-            }, editor, null, 100)
+        
+		CKEDITOR.document.appendStyleSheet(this.path + 'css/wordcount.css');
+
+        function counterId(editor) {
+            return 'cke_wordcount_' + editor.name
         }
+
+        function counterElement(editor) {
+            return document.getElementById(counterId(editor))
+        }
+
+        function strip(html) {
+            var tmp = document.createElement("div");
+            tmp.innerHTML = html;
+            return tmp.textContent || tmp.innerText
+        }
+
+        function updateCounter(editor) {
+            var count = 0;
+            if (editor.getData()) {
+                count = strip(editor.getData()).trim().split(/\s+/).length;
+            }
+            var html = format.replace('%count%', count);
+            counterElement(editor).innerHTML = html
+            if (count == lastCount) {
+                return true
+            } else {
+                lastCount = count
+            }
+            if (count > limit) {
+                limitReached(editor, limitReachedNotified)
+            } else if (!limitRestoredNotified && count < limit) {
+                limitRestored(editor)
+            }
+        }
+
+        function limitReached(editor, notify) {
+            limitReachedNotified = true;
+            limitRestoredNotified = false;
+            editor.execCommand('undo');
+            if (!notify) {
+                editor.fire('limitReached', {}, editor);
+            }
+            // lock editor
+            editor.config.Locked = 1;
+            editor.fire("change");
+        }
+
+        function limitRestored(editor) {
+            limitRestoredNotified = true;
+            limitReachedNotified = false;
+            editor.config.Locked = 0;
+        }
+        editor.on('uiSpace', function (event) {
+            if (event.data.space == 'bottom') {
+                event.data.html += '<div id="' + counterId(event.editor) + '" class="cke_wordcount" style=""' + ' title="' + CKEDITOR.tools.htmlEncode('Words Counter') + '"' + '>&nbsp;</div>'
+            }
+        }, editor, null, 100);
+        editor.on('instanceReady', function (event) {
+            if (editor.config.wordcount_limit != undefined) {
+                limit = editor.config.wordcount_limit
+            }
+            if (editor.config.wordcount_format != undefined) {
+                format = editor.config.wordcount_format
+            }
+        }, editor, null, 100);
+        editor.on('dataReady', function (event) {
+            var count = event.editor.getData().length;
+            if (count > limit) {
+                limitReached(editor)
+            }
+            updateCounter(event.editor)
+        }, editor, null, 100);
+        editor.on('change', function (event) {
+            updateCounter(event.editor)
+        }, editor, null, 100);
+        editor.on('focus', function (event) {
+            editorHasFocus = true;
+            intervalId = window.setInterval(function (editor) {
+                updateCounter(editor)
+            }, 1000, event.editor)
+        }, editor, null, 100);
+        editor.on('blur', function (event) {
+            editorHasFocus = false;
+            if (intervalId) clearInterval(intervalId)
+        }, editor, null, 100)
     }
 });
