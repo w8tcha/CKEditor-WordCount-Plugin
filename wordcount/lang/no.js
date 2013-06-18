a85594451115eca105c51324678c862c371b3312
Vegard S <jvstene@gmail.com>
2013-06-18 10:46:12 +0200

    Added Norwegian translation

---
+++ wordcount/lang/no.js
@@ -0,0 +1,7 @@
+﻿// Norwegian translation by Vegard S.
+CKEDITOR.plugins.setLang('wordcount', 'no', {
+    WordCount: 'Ord:',
+    CharCount: 'Tegn:',
+    limit: 'Grense:',
+    title: 'Statistikk'
+});
