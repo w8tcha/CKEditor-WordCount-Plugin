6c5dd310b3f5018b686282d8ea72768f62d4de5d
Ingo Herbote <thewatcher@watchersnet.de>
2014-02-25 13:21:42 +0100

    fixed #52

--- wordcount/lang/pt-BR.js
+++ wordcount/lang/pt-BR.js
@@ -2,9 +2,10 @@
 Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang('wordcount', 'pt-BR', {
-            WordCount: 'Contagem de palavras:',
-            CharCount: 'Contagem de caracteres:',
-            limit: 'Limit:',
-             title: 'Estatísticas'
-     });
+CKEDITOR.plugins.setLang('wordcount', 'pt-br', {
+    WordCount: 'Contagem de palavras:',
+	CharCount: 'Contagem de caracteres:',
+    CharCountWithHTML: 'Characters (including HTML):',
+    limit:'Limit:',
+    title: 'Estatísticas'
+});
