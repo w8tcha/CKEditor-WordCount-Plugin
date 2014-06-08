2d7c0bb2e776fe50538db95d2bdcb9eb0d43b387
Ingo Herbote <thewatcher@watchersnet.de>
2014-06-08 05:17:07 +0200

    updated brasilian language

--- wordcount/lang/pt-br.js
+++ wordcount/lang/pt-br.js
@@ -6,6 +6,6 @@ CKEDITOR.plugins.setLang('wordcount', 'pt-br', {
     WordCount: 'Contagem de palavras:',
 	CharCount: 'Contagem de caracteres:',
     CharCountWithHTML: 'Characters (including HTML):',
-    limit:'Limit:',
+    limit:'Limite:',
     title: 'Estatísticas'
 });
