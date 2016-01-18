913042e13dbeff13cd31c4a3caab2319e5b96420
Nuno Pereira <nuni21@gmail.com>
2016-01-18 16:56:29 +0000

    Create pt-pt.js
    Added pt-pt lang

---
+++ wordcount/lang/pt-pt.js
@@ -0,0 +1,9 @@
+CKEDITOR.plugins.setLang('wordcount', 'pt-pt', {
+    WordCount: 'Palavras:',
+    CharCount: 'Carateres:',
+    CharCountWithHTML: 'Carateres (incluindo HTML):',
+    Paragraphs: 'Parágrafos:',
+    pasteWarning: 'Conteúdo não pode ser colado porque ultrapassa o limite permitido',
+    Selected: 'Selecionado: ',
+    title: 'Estatísticas'
+});
