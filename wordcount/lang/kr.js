7958945e9904c4c6708447dd3289a4cec4a8524e
Maxime Houdais <houdais.m@gmail.com>
2018-05-12 14:14:31 +0200

    Add Korean translation

---
+++ wordcount/lang/kr.js
@@ -0,0 +1,15 @@
+/*
+Korean translation by Maxime Houdais
+*/
+CKEDITOR.plugins.setLang('wordcount', 'kr', {
+    WordCount: '단어:',
+    WordCountRemaining: '남은 단어',
+    CharCount: '글자:',
+    CharCountRemaining: '남은 글자',
+    CharCountWithHTML: '글자 와 HTML:',
+    CharCountWithHTMLRemaining: '남은 글자 와 HTML',
+    Paragraphs: '단락:',
+    pasteWarning: '허용 된 한도를 초과하여 콘텐츠를 붙여 넣을 수 없습니다.',
+    Selected: '선택:',
+    title: '통계'
+});
