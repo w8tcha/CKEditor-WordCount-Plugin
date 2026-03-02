23574c5566df4657ada03225470964939d2e2617
Alius <aliusa@users.noreply.github.com>
2026-03-02 15:33:27 +0200

    Add Lithuanian language support for word count

---
+++ wordcount/lang/lt.js
@@ -0,0 +1,17 @@
+/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang('wordcount', 'lt', {
+    WordCount: 'Žodžiai:',
+    WordCountRemaining: 'Liko žodžių',
+    CharCount: 'Simbolių:',
+    CharCountRemaining: 'Liko simbolių',
+    CharCountWithHTML: 'Simbolių (su HTML):',
+    CharCountWithHTMLRemaining: 'Liko simbolių (su HTML)',
+    Paragraphs: 'Paragrafai:',
+    ParagraphsRemaining: 'Liko paragrafų',
+    pasteWarning: 'Turinys negali būti įklijuotas, nes viršija nustatytą limitą',
+    Selected: 'Pasirinkta: ',
+    title: 'Statistika'
+});
